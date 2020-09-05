b9e4aac3bf743d6857edd90e57b49b68411a1de9
Elionor Cordova <elicg2012@hotmail.com>
2020-09-05 18:42:34 -0400

    Server: Added the corresponding domain model for the Project.

---
+++ Server/Server/Domain/Ingredient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Domain
+{
+    public class Ingredient
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Ingredient(long id, string name, double price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
---
+++ Server/Server/Domain/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Domain
+{
+    public class Pizza
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public IList<Ingredient> Ingredients { get; set; }
+        public double Price { get; set; }
+
+        public Pizza(long id, string name, IList<Ingredient> ingredients, double price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Ingredients = ingredients;
+            this.Price = price;
+        }
+    }
+}
