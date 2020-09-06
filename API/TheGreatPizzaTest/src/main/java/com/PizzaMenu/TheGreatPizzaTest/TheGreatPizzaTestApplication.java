4505be6521b38f18deb728877bdd8aa73fa75a5a
Elionor Cordova <elicg2012@hotmail.com>
2020-09-06 13:32:33 -0400

    API-Service: Server springboot project created.

---
+++ API/TheGreatPizzaTest/src/main/java/com/PizzaMenu/TheGreatPizzaTest/TheGreatPizzaTestApplication.java
@@ -0,0 +1,16 @@
+package com.PizzaMenu.TheGreatPizzaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class TheGreatPizzaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGreatPizzaTestApplication.class, args);
+	}
+	
+
+}
