7e25b0b96e4fdd52578c12793a213f8fde3c6487
Elionor Cordova <Elionor.Cordova@aumentumtech.com>
2020-09-06 23:44:00 -0400

    API-Service: Enabled Cors.

--- API/TheGreatPizzaTest/src/main/java/com/PizzaMenu/TheGreatPizzaTest/TheGreatPizzaTestApplication.java
+++ API/TheGreatPizzaTest/src/main/java/com/PizzaMenu/TheGreatPizzaTest/TheGreatPizzaTestApplication.java
@@ -1,9 +1,16 @@
 package com.PizzaMenu.TheGreatPizzaTest;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class TheGreatPizzaTestApplication {
@@ -12,5 +19,17 @@ public class TheGreatPizzaTestApplication {
 		SpringApplication.run(TheGreatPizzaTestApplication.class, args);
 	}
 	
-
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.setAllowedOrigins(Collections.singletonList("*"));
+	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+	    source.registerCorsConfiguration("/**", config);
+	    
+	    return new CorsFilter(source);
+	}
+	
 }
